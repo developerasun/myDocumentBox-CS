d56421f56cd106927da29e36e154a1ac938bb79b
designerasun <asuntheawesome@gmail.com>
2022-02-27 17:12:14 +0900

    feat : add a node at certain position

---
+++ 자료구조/reference/code/linkedList/insertion/atCertainPosition.cpp
@@ -0,0 +1,22 @@
+// exercise : create a single linked list with three nodes
+// and add a node at 3rd place.
+// before : node 1 node 2 node 3
+// after : node 1 node 2 (inserted new node 3) node 4
+// initial data set : 1, 2, 4
+// position : 3
+
+#include <corecrt_malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+struct node
+{
+    int data;
+    struct node* link;
+};
+
+
+int main() {
+    // tried to implement with double pointer but failed
+    // retry later 
+    return 0;
+}
