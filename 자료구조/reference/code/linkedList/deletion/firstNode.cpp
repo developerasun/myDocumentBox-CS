54730193782f9cf2c7d344a2e0d2b045fe9b5595
designerasun <asuntheawesome@gmail.com>
2022-02-27 17:21:48 +0900

    chore : add linked list deletion exercises

---
+++ 자료구조/reference/code/linkedList/deletion/firstNode.cpp
@@ -0,0 +1,5 @@
+// exercise : create a single linked list with three nodes
+// and remove a node at the beginning of it
+// be cautious that head should be moved first
+// before removing first node
+
---
+++ 자료구조/reference/code/linkedList/deletion/lastNode.cpp
@@ -0,0 +1,4 @@
+// exercise : create a single linked list with three nodes
+// and remove a node at the end of it
+// be cautious that head should be moved first
+// before removing first node
